329116d9f2b1dfc03b6bf6f550a806daaa74982c
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-09-12 15:28:52 +0600

    1 file modified

--- src/app/user-module/user.module.ts
+++ src/app/user-module/user.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { UserComponent } from './component/user/user.component';
 import { CommonService } from './service/common.service';
-import { LoggerService } from './service/logger.service';
-import { EvenBetterLoggerService } from './service/even-better-logger.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
