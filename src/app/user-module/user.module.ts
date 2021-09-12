0ec483ca4da63eed3079adf21a267b5341d62caf
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-09-12 15:44:40 +0600

    1 file modified

--- src/app/user-module/user.module.ts
+++ src/app/user-module/user.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { UserComponent } from './component/user/user.component';
 import { CommonService } from './service/common.service';
+import { EvenBetterLoggerService } from './service/even-better-logger.service';
+import { LoggerService } from './service/logger.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [UserComponent],
-  exports: [UserComponent]
+  exports: [UserComponent],
+  providers: [{ provide: LoggerService, useClass: EvenBetterLoggerService }]
 })
 export class UserModule {}
